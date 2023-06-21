287eb37254962f50c117a9375430ac513144fac0
mohanrajmurus <mohanrajmurus@gmail.com>
2023-06-21 13:23:41 +0530

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const menuBar = document.getElementById('menu--bar');
 const navBar = document.querySelector('.header--nav__bar')
 const closeMenu = document.querySelector('.close--menu')
+const menuLinks = document.querySelectorAll('.nav--item');
+
 menuBar.addEventListener('click',() => {
     navBar.classList.add('side--menu')
     closeMenu.classList.add('close')
@@ -8,4 +10,10 @@ menuBar.addEventListener('click',() => {
 closeMenu.addEventListener('click',()=>{
     navBar.classList.remove('side--menu')
     closeMenu.classList.remove('close')
+})
+menuLinks.forEach(item => {
+    item.addEventListener('click',()=>{
+        navBar.classList.remove('side--menu')
+        closeMenu.classList.remove('close')
+    })
 })
